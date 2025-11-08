07a3249204bf30c25575d814ff12c1188d6023ff
MarcAbouSerhal <marcabouserhal@gmail.com>
2025-11-08 21:42:52 +0200

    extract_scc_features now takes a mask, changed features a bit

--- algorithms.cpp
+++ algorithms.cpp
@@ -25,20 +25,20 @@ static void find_longest_path(adjacency_list& adj, bool_grid& dp) {
 // Requirements:
 //  `id` must be initialized to -1s
 //  `s` and `t` must be initialized to 0s
-vector<vector<int>> getSCCs(adjacency_list& adj, int_array& id, int_array& t, int_array& s, int mask) {
+vector<vector<int>> getSCCs(adjacency_list& adj, int_array& id, int_array& t, int_array& st, int s) {
     int tick = 0, group_id = 0;
     vector<vector<int>> scc;
     auto dfs = [&](int u, auto&& self) -> int {
         int low = t[u] = ++tick;
-        s.add(u);
+        st.add(u);
         for(int v: adj[u])
-            if(id[v] == -1 && contains(mask, v))
+            if(id[v] == -1 && contains(s, v))
                 low = min(low, t[v] != 0 ? t[v] : self(v, self));
         if(low == t[u]) {
             int v;
             scc.push_back(vector<int>());
             while(true) {
-                id[v = s.pop()] = group_id;
+                id[v = st.pop()] = group_id;
                 scc[group_id].push_back(v);
                 if(v == u) break;
             }
@@ -47,12 +47,12 @@ vector<vector<int>> getSCCs(adjacency_list& adj, int_array& id, int_array& t, in
         return low;
     };
     for(int u = 0; u < N; ++u)
-        if(t[u] == 0 && contains(mask, u))
+        if(t[u] == 0 && contains(s, u))
             dfs(u, dfs);
     return scc;
 }
 
-// Returns mask of all reachable vertices from `u` in the graph induced by `s`
+// Returns mask of all reachable vertices from `u` 
 int reachable(adjacency_list& adj, int u, int s) {
     int mask = 1 << u;
     auto dfs = [&](int u_, auto&& self) -> void {
--- feature_calculation.cpp
+++ feature_calculation.cpp
@@ -12,22 +12,24 @@ struct vertex_features {
     int outdegree_inside_scc = 0;
     int outdegree_outside_scc = 0;
     int indegree_inside_scc = 0;
-    int longest_shortest_path_inside_scc;
-    int longest_path_using_shortest_paths;
-    int first_shortest_path_used;
+    int longest_dfs_path_inside_scc;
+    int longest_path_using_dfs_paths;
+    int first_dfs_path_used;
     scc_features* scc_feats = NULL;
 };
 
 // Returns `vector` of `scc_feature` structs for each SCC
-vector<scc_features> extract_scc_features(vector<vector<int>>& scc, int_array& id, adjacency_list& adj) {
+vector<scc_features> extract_scc_features(vector<vector<int>>& scc, int_array& id, adjacency_list& adj, int s) {
     int k = scc.size();
     vector<scc_features> scc_feats(k);
     for(int i = 0; i < k; ++i) {
         scc_features& this_scc_feats = scc_feats[i];
         this_scc_feats.size = scc[i].size();
         this_scc_feats.largest_path_sum = this_scc_feats.size;
-        for(int u: scc[i])
-            for(int v: adj[u])
+        for(int u: scc[i]) {
+            if(!contains(s, u)) continue;
+            for(int v: adj[u]) {
+                if(!contains(s, v)) continue;
                 if(id[u] == id[v]) {
                     ++this_scc_feats.number_of_edges_inside_scc;
                 }
@@ -38,6 +40,8 @@ vector<scc_features> extract_scc_features(vector<vector<int>>& scc, int_array& i
                         this_scc_feats.size + scc_feats[id[v]].largest_path_sum
                     );
                 }
+            }
+        }
     }
     return scc_feats;
 }
